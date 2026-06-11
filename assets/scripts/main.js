e98131d8d7ac2e8418f0f016fe05763b3bdca1a5
Caton Tan <z5478613@ad.unsw.edu.au>
2026-06-11 19:18:56 +1000

    everything moved onto index.html

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -34,40 +34,8 @@ setInterval(updateClock, 1000);
 
 
 const enterBtn = document.getElementById('enter-btn');
-const section = document.getElementById('content');
 
-if (enterBtn && section) {
-    enterBtn.addEventListener('click', () => {
-        section.classList.toggle('open');
-        enterBtn.textContent = section.classList.contains('open') ? 'close' : 'drop down';
-    });
-}
-
-
-// Modal Script
-const modal = document.getElementById("myModal");
-const buttons = document.querySelectorAll(".modalBtn");
-const closeBtn = document.querySelector(".close");
-
-console.log("Modal:", modal);
-console.log("Buttons:", buttons);
-console.log("Close button:", closeBtn);
-
-buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
-        console.log("Button clicked!");
-        modal.style.display = "block";
-    });
-});
-
-if (closeBtn) {
-    closeBtn.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
-}
-
-window.addEventListener("click", (e) => {
-    if (e.target === modal) {
-        modal.style.display = "none";
-    }
+enterBtn.addEventListener('click', () => {
+    document.getElementById('landing').classList.add('hidden');
+    document.getElementById('bar-scene').classList.remove('hidden');
 });
