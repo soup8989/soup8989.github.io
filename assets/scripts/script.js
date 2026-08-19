6ded38515c3a007645b32aa99f1922617a1529a3
soup8989 <catontan7@gmail.com>
2026-08-19 22:50:09 +0000

    threejs!

---
+++ assets/scripts/script.js
@@ -0,0 +1,37 @@
+import * as THREE from 'three'
+
+console.log("Javascript is working")
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+console.log(canvas)
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Render
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
